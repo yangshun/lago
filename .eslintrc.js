1707bf7859ba23b56d9c102ff51060f508e8ffc7
Brandon Zheng <imbrandonzheng@gmail.com>
2017-10-23 00:46:44 +0800

    Add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
-  extends: 'airbnb-base',
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
   env: {
     browser: true,
     jest: true,
     node: true,
   },
+  plugins: [
+    'prettier',
+  ],
   rules: {
     'no-param-reassign': 'off', // Needed for swapping elements within an array.
     'no-plusplus': 'off',
     'no-underscore-dangle': 'off', // Indicate private methods.
+    'prettier/prettier': 'error',
   },
 };
+
