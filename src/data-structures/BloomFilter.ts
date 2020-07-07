9f12b16224e69e99e3d4420a3f61a934780ad6f9
Yangshun Tay <tay.yang.shun@gmail.com>
2020-07-07 13:28:15 +0800

    feat: TypeScript-ify Bloom Filter

--- src/data-structures/BloomFilter.ts
+++ src/data-structures/BloomFilter.ts
@@ -3,38 +3,43 @@
   It may not be all-encompassing, but it's better than none.
   All numbers used are prime numbers.
 */
-// (ax + b) % p
-const defaultHashFunctions = [
-  (x) => (25 * x + 13) % 31,
-  (x) => (109 * x + 71) % 139,
-  (x) => (677 * x + 241) % 859,
-  (x) => (547 * x + 383) % 997,
-  (x) => (173 * x + 149) % 499,
-];
 
-class BloomFilter {
+export type HashFunctions<T> = ReadonlyArray<(x: T) => number>;
+
+class BloomFilter<T> {
+  private _bits: Array<boolean>;
+
+  private _size: number;
+
+  private _hashFunctions: HashFunctions<T>;
+
+  static numberHashFunctions: HashFunctions<number> = [
+    (x: number): number => (25 * x + 13) % 31,
+    (x: number): number => (109 * x + 71) % 139,
+    (x: number): number => (677 * x + 241) % 859,
+    (x: number): number => (547 * x + 383) % 997,
+    (x: number): number => (173 * x + 149) % 499,
+  ];
+
   /**
    * Initialize a bloom filter with an appropriate size
    * and array of hash functions.
+   *
    * @param {number} size The size of bloom filter to initialize as.
    * @param {Function[]} hashFunctions The array of hash functions to use.
    * @return {undefined}
    */
-  constructor(size, hashFunctions) {
+  constructor(size: number, hashFunctions: HashFunctions<T>) {
     this._bits = Array(size).fill(false);
     this._size = size;
-    if (hashFunctions) {
-      this._hashFunctions = hashFunctions;
-    } else {
-      this._hashFunctions = defaultHashFunctions;
-    }
+    this._hashFunctions = hashFunctions;
   }
 
   /**
    * Add an item to the bloom filter to register its membership.
    * @param {*} item The item to be added.
    */
-  add(item) {
+  add(item: T): void {
     this._hashFunctions.forEach((hashFunction) => {
       // the mod _size is needed to ensure we do not go out of bounds
       this._bits[hashFunction(item) % this._size] = true;
@@ -47,11 +52,19 @@ class BloomFilter {
    * @return {boolean} Whether the item is probably in the set or not.
    *        If a false is returned, the item is surely not in the set.
    */
-  contains(item) {
+  contains(item: T): boolean {
     // check if every bit at the location indicated by the hash function returns true
     return this._hashFunctions.every(
       (hashFunction) => this._bits[hashFunction(item) % this._size],
     );
+  }
+
+  /**
+   * Returns the size of the bloom filter.
+   * @return {number} Size of bloom filter.
+   */
+  get size(): number {
+    return this._size;
   }
 }
 
---
+++ src/data-structures/__tests__/BloomFilter.test.ts
@@ -0,0 +1,37 @@
+import type { HashFunctions } from '../BloomFilter';
+
+import BloomFilter from '../BloomFilter';
+
+const OBJECT_HASH_FUNCTIONS: HashFunctions<Readonly<{ value: number }>> = [
+  (x) => (25 * x.value + 13) % 31,
+  (x) => (109 * x.value + 71) % 139,
+  (x) => (677 * x.value + 241) % 859,
+  (x) => (547 * x.value + 383) % 997,
+  (x) => (173 * x.value + 149) % 499,
+];
+
+describe('BloomFilter', () => {
+  test('constructor()', () => {
+    const bf = new BloomFilter(20, BloomFilter.numberHashFunctions);
+    expect(bf).toBeTruthy();
+    expect(bf.size).toBe(20);
+  });
+
+  describe('add() and contains()', () => {
+    test('using numbers only', () => {
+      const bf = new BloomFilter(20, BloomFilter.numberHashFunctions);
+      bf.add(5);
+
+      expect(bf.contains(5)).toBe(true);
+      expect(bf.contains(7)).toBe(false);
+    });
+
+    test('using objects and custom hash functions', () => {
+      const bf = new BloomFilter(20, OBJECT_HASH_FUNCTIONS);
+      bf.add({ value: 20 });
+
+      expect(bf.contains({ value: 20 })).toBe(true);
+      expect(bf.contains({ value: 9 })).toBe(false);
+    });
+  });
+});
