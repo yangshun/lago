de31b4829feb8f9b3bb96aea80d7b9262717125c
utkarsh261 <utkarshpandey924@gmail.com>
2020-07-16 04:06:38 +0530

    Added delete method for Binary Search Tree

--- src/data-structures/BinarySearchTree.ts
+++ src/data-structures/BinarySearchTree.ts
@@ -43,7 +43,6 @@ class BinarySearchTree extends BinaryTree<number> {
   /**
    * Recursively search for a value in the BST
    * @param {number} value The value to search for.
-   * @param {*} node The current node.
    * @return {Boolean} True if value is found, false if not.
    */
   search(val: number): boolean {
@@ -116,6 +115,61 @@ class BinarySearchTree extends BinaryTree<number> {
     }
 
     return getMaximumImpl(this.root);
+  }
+
+  /**
+   * Recursively delete a given value from the tree.
+   * @param {number} value The value to delete.
+   * @param {BinaryTreeNode} node The current node.
+   * @return {BinaryTreeNode} The root node after deletion.
+   */
+  delete(val: number): BinaryTreeNode<number> | null {
+    function deleteImpl(
+      value: number,
+      node: BinaryTreeNode<number> | null,
+    ): BinaryTreeNode<number> | null {
+      if (!node) {
+        return null;
+      }
+
+      const nodeValue = node.value;
+      const { left } = node;
+      const { right } = node;
+      if (value < nodeValue) {
+        node.left = deleteImpl(value, left);
+        return node;
+      } else if (value > nodeValue) {
+        node.right = deleteImpl(value, right);
+        return node;
+      } else {
+        if (!left && !right) {
+          node = null;
+          return node;
+        }
+        if (!left) {
+          node = right;
+          return node;
+        } else if (!right) {
+          node = left;
+          return node;
+        }
+        const tempNode: BinaryTreeNode<number> = findMinNodeHelper(right);
+        node.value = tempNode.value;
+        node.right = deleteImpl(tempNode.value, right);
+        return node;
+      }
+    }
+    function findMinNodeHelper(
+      node: BinaryTreeNode<number>,
+    ): BinaryTreeNode<number> {
+      const { left } = node;
+      if (!left) {
+        return node;
+      } else {
+        return findMinNodeHelper(left);
+      }
+    }
+    return (this.root = deleteImpl(val, this.root));
   }
 }
 
--- test/data-structures/BinarySearchTree.test.ts
+++ test/data-structures/BinarySearchTree.test.ts
@@ -66,4 +66,128 @@ describe('BinarySearchTree', () => {
       expect(tree.getMaximum()).toBe(15);
     });
   });
+
+  describe('delete()', () => {
+    test('delete node from single-node BST', () => {
+      const tree = new BinarySearchTree(20);
+      tree.delete(20);
+      expect(tree.inOrder()).toEqual([]);
+    });
+    test('delete root node from BST', () => {
+      const tree = new BinarySearchTree(30);
+      tree.insert(20);
+      tree.insert(40);
+      tree.insert(10);
+      tree.insert(50);
+      tree.insert(5);
+      tree.insert(6);
+
+      tree.delete(30);
+      expect(tree.inOrder()).toEqual([5, 6, 10, 20, 40, 50]);
+    });
+    test('delete value from BST which does not exists', () => {
+      const tree = new BinarySearchTree(20);
+      tree.insert(10);
+      tree.insert(50);
+      tree.insert(60);
+      tree.insert(40);
+      tree.insert(70);
+      tree.insert(5);
+      tree.insert(6);
+
+      tree.delete(100);
+      expect(tree.inOrder()).toEqual([5, 6, 10, 20, 40, 50, 60, 70]);
+    });
+    test('deletes value from the BST', () => {
+      const tree = new BinarySearchTree(50);
+      tree.insert(20);
+      tree.insert(10);
+      tree.insert(5);
+      tree.insert(15);
+      tree.insert(12);
+      tree.insert(14);
+      tree.insert(30);
+      tree.insert(35);
+      tree.insert(40);
+      tree.insert(60);
+      tree.insert(55);
+      tree.insert(80);
+      tree.insert(70);
+      tree.insert(100);
+
+      tree.delete(100);
+      expect(tree.inOrder()).toEqual([
+        5,
+        10,
+        12,
+        14,
+        15,
+        20,
+        30,
+        35,
+        40,
+        50,
+        55,
+        60,
+        70,
+        80,
+      ]);
+      tree.delete(10);
+      expect(tree.inOrder()).toEqual([
+        5,
+        12,
+        14,
+        15,
+        20,
+        30,
+        35,
+        40,
+        50,
+        55,
+        60,
+        70,
+        80,
+      ]);
+      tree.delete(80);
+      expect(tree.inOrder()).toEqual([
+        5,
+        12,
+        14,
+        15,
+        20,
+        30,
+        35,
+        40,
+        50,
+        55,
+        60,
+        70,
+      ]);
+      tree.delete(20);
+      expect(tree.inOrder()).toEqual([
+        5,
+        12,
+        14,
+        15,
+        30,
+        35,
+        40,
+        50,
+        55,
+        60,
+        70,
+      ]);
+      tree.delete(15);
+      tree.delete(60);
+      expect(tree.inOrder()).toEqual([5, 12, 14, 30, 35, 40, 50, 55, 70]);
+      tree.delete(35);
+      tree.delete(12);
+      expect(tree.inOrder()).toEqual([5, 14, 30, 40, 50, 55, 70]);
+      tree.delete(30);
+      expect(tree.inOrder()).toEqual([5, 14, 40, 50, 55, 70]);
+      tree.delete(40);
+      tree.delete(50);
+      expect(tree.inOrder()).toEqual([5, 14, 55, 70]);
+    });
+  });
 });
